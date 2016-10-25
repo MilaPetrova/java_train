131e4890a5f9d6c55fb5bd8892dd1f4dc48288f9
MilaPetrova <lvptrv83@gmail.com>
2016-10-24 23:40:19 -0400

    setup on home PC

--- sandbox/src/main/java/intreposit1/sandbox/TestOne.java
+++ sandbox/src/main/java/intreposit1/sandbox/TestOne.java
@@ -2,6 +2,7 @@ package intreposit1.sandbox;
 
 public class TestOne {
  public static void main (String[] args){
-	System.out.println("Hello World!");
- }
+     System.out.println("Hello World!");
+     System.out.println(2+2);
+      }
 }
