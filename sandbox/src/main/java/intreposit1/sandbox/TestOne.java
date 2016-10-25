1ace331381d29f95acf6a2a717f07976de039806
MilaPetrova <mpetrova@aliaswire.com>
2016-10-25 17:45:29 -0400

    project indexed
    # Conflicts:
    #	sandbox/.idea/gradle.xml
    #	sandbox/.idea/misc.xml
    #	sandbox/.idea/workspace.xml
    #	sandbox/src/main/java/intreposit1/sandbox/TestOne.java

--- sandbox/src/main/java/intreposit1/sandbox/TestOne.java
+++ sandbox/src/main/java/intreposit1/sandbox/TestOne.java
@@ -4,5 +4,5 @@ public class TestOne {
  public static void main (String[] args){
      System.out.println("Hello World!");
      System.out.println(2+2);
-      }
+     }
 }
---
+++ sandbox/src/test/java/intreposit1/sandbox/SquareTests.java
@@ -0,0 +1,7 @@
+package intreposit1.sandbox;
+
+/**
+ * Created by mpetrova on 10/25/2016.
+ */
+public class SquareTests {
+}
