532619e39a4f75475dcd0ed72f21282aee19260d
MilaPetrova <mpetrova@aliaswire.com>
2016-11-08 14:42:33 -0500

    dependencies are added to gradle config

--- sandbox/src/main/java/intreposit1/sandbox/Rectangle.java
+++ sandbox/src/main/java/intreposit1/sandbox/Rectangle.java
@@ -6,10 +6,14 @@ package intreposit1.sandbox;
 public class Rectangle {
   public double a;
   public double b;
-}
 
   public Rectangle(double a, double b) {
     this.a = a;
     this.b = b;
+  }
+
+  public double area() {
+
+    return this.a * this.b;
   }
 }
--- sandbox/src/main/java/intreposit1/sandbox/TestOne.java
+++ sandbox/src/main/java/intreposit1/sandbox/TestOne.java
@@ -6,21 +6,18 @@ public class TestOne {
     System.out.println(2 + 2);
 
     Square s=new Square(5);
-      System.out.println("Area of square with side "+s.l+" = "+area(s));
+      System.out.println("Area of square with side "+s.l+" = "+s.area());
 
-    Rectangle r=new Rectangle();
-    r.a=5;
-    r.b=3;
-    System.out.println("Area of rectangle with sides "+r.a+" and "+r.b+" = "+area(r));
+    Rectangle r=new Rectangle(5, 3);
+    System.out.println("Area of rectangle with sides "+r.a+" and "+r.b+" = "+ r.area());
 
   }
 
 
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
+
 
   public static double area(Rectangle r) {
+
     return r.a * r.b;
   }
 }
